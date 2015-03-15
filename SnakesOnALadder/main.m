6fdccd10592e14b9eec8814b0daf404e907d4982
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-03-14 22:52:51 -0700

    Initial Commit

---
+++ SnakesOnALadder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesOnALadder
+//
+//  Created by Jeffrey C Rosenthal on 3/14/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
